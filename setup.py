707457e86179a7ef51cd748b498ef096cbc3230a
Kay Hau <virtualda@gmail.com>
2022-08-27 15:36:52 +1000

    Updated codecov

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ except (ImportError, IOError, OSError) as e:
 
 CLASSIFIERS = [
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
